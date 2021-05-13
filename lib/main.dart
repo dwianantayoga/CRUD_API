59e9365df58add90fc87db84527a526aa4556171
dwianantayoga <dwi.ananta@undiksha.ac.id>
2021-05-14 00:09:59 +0800

    progres pertama crud

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:crud/screen/halaman_beranda.dart';
+import 'package:crud/screen/halaman_tambah_edit.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Aplikasi Angkringan',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
+      ),
+      initialRoute: HalamanBeranda.id,
+      routes: {
+        HalamanBeranda.id: (context) => HalamanBeranda(),
+        HalamanTambahEdit.id: (context) => HalamanTambahEdit()
+      },
+    );
+  }
+}
